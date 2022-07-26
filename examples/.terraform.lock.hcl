9744e1007ba0dbee8aadb24b99a52c0b303fe64b
Ibrahim Khawar <ikb1703@gmail.com>
2022-07-26 19:15:40 -0400

    fixed issue with data source

---
+++ examples/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "logisense.com/service/logiprovider" {
+  version     = "0.2.0"
+  constraints = "0.2.0"
+  hashes = [
+    "h1:IjWbJ5IpDBh2PyYaeiaB/YulKS2Rmwdh32A76p7SmGk=",
+  ]
+}
---
+++ examples/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    logiprovider = {
+      version = "0.2"
+      source  = "logisense.com/service/logiprovider"
+    }
+  }
+}
+
+provider "logiprovider" {
+  username = "admin"
+  password = "admin"
+  client_id = "044b8ad6006845c29446b2f18e5b5909"
+  host = "https://vnexttrainingps.dev.logisensebilling.com"
+}
+
+data "logiprovider_service" "all" {}
